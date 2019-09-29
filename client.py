465331e87c277143e1418aca4e3d1a5ba1b4d4f0
Arutyun <arutyun.mnatsakanyan@gmail.com>
2019-09-29 06:02:59 +0300

    fix

--- client.py
+++ client.py
@@ -6,9 +6,10 @@ def send_barcode(barcode):
     print('before', barcode)
     barcode = barcode.strip().replace('^[[B', '')
     print('after', barcode)
+    print(len(barcode))
     r = requests.post(
         'https://scancash.herokuapp.com/scanners/record',
         json={
             'scanner_id': scanner_id,
-            'record': barcode.strip().replace('^[[B', ''),
+            'record': barcode,
         })
