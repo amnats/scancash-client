b95a3cc5aa1d50b2c42029b619eecd396bfc4cfe
Arutyun <arutyun.mnatsakanyan@gmail.com>
2019-09-29 14:34:14 +0300

    fix

--- client.py
+++ client.py
@@ -4,11 +4,15 @@ from config import scanner_id
 
 def send_barcode(barcode):
     print('before', barcode)
-    barcode = barcode.strip().replace('^[[B', '')
-    barcode = ''.join(c for c in barcode if c.isprintable())
-    barcode = barcode.replace('[B', '')
-    print('after', barcode)
-    print(len(barcode))
+    try:
+        barcode = barcode.strip().replace('^[[B', '')
+        barcode = ''.join(c for c in barcode if c.isprintable())
+        barcode = barcode.replace('[B', '')
+        print('after', barcode)
+        print(len(barcode))
+    except TypeError:
+        pass
+
     r = requests.post(
         'https://scancash.herokuapp.com/scanners/record',
         json={
